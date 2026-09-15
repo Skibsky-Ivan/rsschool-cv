2f127746e71f6b17f2560b0b78b004213118351d
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-09-15 03:25:05 +0300

    feat(reveal): sections reveal on scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { initHeader } from './header.js';
 import { initBurger } from './burger.js';
 import { initTheme } from './theme.js';
+import { initReveal } from './reveal.js';
 
 initHeader();
 initBurger();
 initTheme();
+initReveal();
